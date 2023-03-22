0052cba25a6fab7156c4a377e367c1d5881e821f
SoyoonPark <spurt@tukore.ac.kr>
2023-03-23 02:20:21 +0900

    init  : 백엔드 기본 세팅 완료

--- src/main/java/com/example/Mindspace_backend/MindspaceBackendApplication.java
+++ src/main/java/com/example/Mindspace_backend/MindspaceBackendApplication.java
@@ -13,8 +13,8 @@ public class MindspaceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MindspaceBackendApplication.class, args);
 	}
-		@GetMapping("/hello")
-		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-			return String.format("Hello %s!", name);
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 }
