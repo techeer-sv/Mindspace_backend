365d39f207632e221308efdc0ad6931efea268df
zyoon <develop_z@naver.com>
2023-04-02 16:49:56 +0900

    feat: user repository

--- src/main/java/com/mindspace/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/mindspace/backend/domain/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.mindspace.backend.domain.user.repository;
 
-public interface UserRepository {
+import com.mindspace.backend.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
