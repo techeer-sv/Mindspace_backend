d391f8f504060d939f9b51d89cc84631e048fa76
Soyoon <spurt@tukorea.ac.kr>
2023-04-09 20:47:26 +0900

    feat: node dto 작성

--- src/main/java/com/mindspace/backend/domain/node/dto/NodeRequestDto.java
+++ src/main/java/com/mindspace/backend/domain/node/dto/NodeRequestDto.java
@@ -1,4 +1,24 @@
 package com.mindspace.backend.domain.node.dto;
 
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Column;
+
+@Getter
+@Builder
 public class NodeRequestDto {
+
+    @NotNull
+    private String nodeName;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private int userId;
 }
--- src/main/java/com/mindspace/backend/domain/node/dto/NodeResponseDto.java
+++ src/main/java/com/mindspace/backend/domain/node/dto/NodeResponseDto.java
@@ -1,4 +1,23 @@
 package com.mindspace.backend.domain.node.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Column;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class NodeResponseDto {
+
+    private int id;
+
+    private String nodeName;
+
+    private String title;
+
+    private String content;
+
+    private int userId;
 }
