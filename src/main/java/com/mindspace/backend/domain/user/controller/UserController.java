3ea4b5cfee9d663d49cf557cb86d8fa576af51ee
zyoon <develop_z@naver.com>
2023-04-09 23:30:55 +0900

    feat: 회원가입 api 개발

--- src/main/java/com/mindspace/backend/domain/user/controller/UserController.java
+++ src/main/java/com/mindspace/backend/domain/user/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.mindspace.backend.domain.user.controller;
 
+import com.mindspace.backend.domain.user.dto.UserMapper;
+import com.mindspace.backend.domain.user.dto.UserResponseDto;
+import com.mindspace.backend.domain.user.dto.UserSignupRequestDto;
+import com.mindspace.backend.domain.user.entity.User;
+import com.mindspace.backend.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/user")
 public class UserController {
+    private final UserService USER_SERVICE;
+    private final UserMapper USER_MAPPER;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDto signupUser(
+            @RequestBody UserSignupRequestDto userSignupRequestDto) {
+        User signupResult = USER_SERVICE.signupUser(userSignupRequestDto);
+        return USER_MAPPER.DtoFromEntity(signupResult);
+    }
+
 }
--- src/main/java/com/mindspace/backend/domain/user/service/UserService.java
+++ src/main/java/com/mindspace/backend/domain/user/service/UserService.java
@@ -1,4 +1,22 @@
 package com.mindspace.backend.domain.user.service;
 
-public interface UserService {
+import com.mindspace.backend.domain.user.dto.UserMapper;
+import com.mindspace.backend.domain.user.dto.UserSignupRequestDto;
+import com.mindspace.backend.domain.user.entity.User;
+import com.mindspace.backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository USER_REPOSITORY;
+    private final UserMapper USER_MAPPER;
+
+    @Transactional
+    public User signupUser(UserSignupRequestDto userSignupRequestDto){
+        return USER_REPOSITORY.save(USER_MAPPER.DtoToEntity(userSignupRequestDto));
+    };
 }
