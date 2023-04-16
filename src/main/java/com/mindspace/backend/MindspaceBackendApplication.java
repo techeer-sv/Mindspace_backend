7059b05d406a3cd33790a8c23015b5a324aa9f0c
zyoon <develop_z@naver.com>
2023-04-16 23:16:23 +0900

    feat: local db 연결

--- src/main/java/com/mindspace/backend/MindspaceBackendApplication.java
+++ src/main/java/com/mindspace/backend/MindspaceBackendApplication.java
@@ -13,8 +13,4 @@ public class MindspaceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MindspaceBackendApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
 }
