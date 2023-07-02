cc105637b7d5fae5fb9d639145a46fb94c3460ea
suhyeon <suhyeon3484@naver.com>
2023-07-02 22:26:32 +0900

    hotfix: delete, put의 cors 에러 해결

--- src/main/java/com/mindspace/backend/global/config/WebConfig.java
+++ src/main/java/com/mindspace/backend/global/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
     }
 }
