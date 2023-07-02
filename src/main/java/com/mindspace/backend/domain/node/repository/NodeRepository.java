64c61e8b8b45e54bd01feaf15572d49b592fee8b
Soyoon <spurt@tukorea.ac.kr>
2023-07-02 19:58:25 +0900

    feat : NodeRepository 에 메소드 추가
    - existsById : 주어진 ID가 데이터베이스에 존재하는지 여부를 확인하는 기능
    - findById : 주어진 ID에 해당하는 노드를 데이터베이스에서 찾아 반환하는 기능

--- src/main/java/com/mindspace/backend/domain/node/repository/NodeRepository.java
+++ src/main/java/com/mindspace/backend/domain/node/repository/NodeRepository.java
@@ -3,5 +3,9 @@ package com.mindspace.backend.domain.node.repository;
 import com.mindspace.backend.domain.node.entity.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NodeRepository extends JpaRepository<Node, Integer> {
+    boolean existsById(int id);
+    Optional<Node> findById(int id);
 }
