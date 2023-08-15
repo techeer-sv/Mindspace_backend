5864c0df0611ed372f23c6318954d81a697f9a99
teawon <xodnjsvkvk9@gmail.com>
2023-08-15 17:27:52 +0900

    feat : 노드의 연결관계 연산 추가

--- src/main/java/com/mindspace/backend/domain/node/service/NodeService.java
+++ src/main/java/com/mindspace/backend/domain/node/service/NodeService.java
@@ -12,7 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +31,17 @@ public class NodeService {
     public NodeInfoResponse getNodeInfoWithLinks(int userId) {
         List<Map<String, Object>> nodes = BOARD_REPOSITORY.getNodeListWithWriteStatus(userId);
         List<Link> linksFromNeo4j = NEO4J_NODE_REPOSITORY.findAllLinks();
+
+        // connectCount 계산 및 추가
+        Map<Integer, Long> connectCounts = linksFromNeo4j.stream()
+                .flatMap(link -> Stream.of(link.getSource().intValue(), link.getTarget().intValue())) // Integer로 변환
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        for (Map<String, Object> node : nodes) {
+            Integer nodeId = (Integer) node.get("id");
+            Long count = connectCounts.getOrDefault(nodeId, 0L);
+            node.put("connectCount", count);
+        }
 
         List<NodeLinkDto> linkDTOs = linksFromNeo4j.stream()
                 .map(NODE_MAPPER::linkToDto)
